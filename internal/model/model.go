35ede525a63a9bf1ddc16778b9692cb8cc11cbeb
mrxtoor <eishtein0000@gmail.com>
2025-06-08 02:53:53 +0300

    rss add new feature: load feed, fetch

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,10 +1,12 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type Item struct {
 	Title      string
-	Category   string
+	Categories []string
 	Link       string
 	Date       time.Time
 	Summary    string
@@ -15,16 +17,17 @@ type Source struct {
 	ID        int64
 	Name      string
 	FeedURL   string
+	Priority  int
 	CreatedAt time.Time
 }
 
 type Article struct {
-	ID        int64
-	SourceID  int64
-	Title     string
-	Link      string
-	Summary   string
-	Published time.Time
-	CreatedAt time.Time
-	PostedAt  time.Time
+	ID          int64
+	SourceID    int64
+	Title       string
+	Link        string
+	Summary     string
+	PublishedAt time.Time
+	PostedAt    time.Time
+	CreatedAt   time.Time
 }
